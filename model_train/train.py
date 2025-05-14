b1dcc15876ee96e6b0f5454a2ed8760e60e9fe68
Gardain <ujl201@bk.ru>
2025-05-14 20:37:48 +0300

    Creating a structure

---
+++ model_train/train.py
@@ -0,0 +1,13 @@
+# import torch
+from ultralytics import YOLO
+
+model = YOLO("yolo11s-seg.pt")
+
+result = model.train(
+    data='D:/Projects/HackInHome2024/model_train/dataset/data.yaml',
+    epochs=250,             
+    imgsz=640,              
+    batch=16,               
+    name='car_segmentation',
+    cache=True              
+)
